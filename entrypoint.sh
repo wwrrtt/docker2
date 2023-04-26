6ab267220ed7db49bbe29aeb1c8dde7d94f37c1d
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-04-26 13:27:15 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 export Token=${Token:-'eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiYmUwYTQ4MTQtZmE2Ny00ZjUyLTlhMTEtY2QxMjgzZWIyMzljIiwicyI6IlkyTmhaamxrTkdZdFlUbGpaaTAwWldSaExXSXhNVGt0TnpZME5qUXdaRFl6TnpkaCJ9'}
 
-./argo.sh tunnel --edge-ip-version auto run --token $Token && ./web.sh -config ./config.json
+nohup ./argo.sh tunnel --edge-ip-version auto run --token $Token  >/dev/null 2>&1 &
+nohup ./web.sh run ./config.json >/dev/null 2>&1 &
