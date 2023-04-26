2d293a80137eba578afeb3e0e0bf846ece5471c7
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-04-26 08:37:47 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,78 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
-SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
-sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" config.json
-sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJAN_WSPATH#${TROJAN_WSPATH}#g;s#SS_WSPATH#${SS_WSPATH}#g" /etc/nginx/nginx.conf
-sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
+export PORT=${PORT-8080}
+export UUID=${UUID:-'1bb7c055-8790-4argo.sh4-da53-c9f9bcc669bd'}
+export VMESS_WSPATH=${VMESS_WSPATH:-'/ray272449844'}
 
-# 设置 nginx 伪装站
-rm -rf /usr/share/nginx/*
-wget https://github.com/wwrrtt/doprax/raw/main/web.zip -O /usr/share/nginx/web.zip
-unzip -o "/usr/share/nginx/web.zip" -d /usr/share/nginx/html
-rm -f /usr/share/nginx/web.zip
+cat << EOF >config.json
+{
+	"log": {
+		"disabled": false,
+		"level": "info",
+		"output": "/dev/null",
+		"timestamp": true
+	},
+	"dns": {
+		"servers": [
+			{
+				"tag": "google-tls",
+				"address": "local",
+				"address_strategy": "prefer_ipv4",
+				"strategy": "ipv4_only",
+				"detour": "direct"
+			},
+			{
+				"tag": "google-udp",
+				"address": "8.8.8.8",
+				"address_strategy": "prefer_ipv4",
+				"strategy": "prefer_ipv4",
+				"detour": "direct"
+			}
+		],
+		"strategy": "prefer_ipv4",
+		"disable_cache": false,
+		"disable_expire": false
+	},
+	"inbounds": [
+		{
+			"type": "vmess",
+			"tag": "vmess-in",
+			"listen": "0.0.0.0",
+			"listen_port": $PORT,
+			"tcp_fast_open": false,
+			"sniff": false,
+			"sniff_override_destination": false,
+			"domain_strategy": "prefer_ipv4",
+			"proxy_protocol": false,
+			"users": [
+				{
+					"name": "kid",
+					"uuid": "$UUID",
+					"alterId": 0
+				}
+			],
+		"transport": {
+        "type": "ws",
+        "path": "$VMESS_WSPATH",
+        "max_early_data": 0,
+        "early_data_header_name": "Sec-WebSocket-Protocol"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "type": "direct",
+      "tag": "direct"
+    }
+  ]
+}
+EOF
 
-# 伪装 xray 执行文件
-RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
-mv xray ${RELEASE_RANDOMNESS}
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 cat config.json | base64 > config
 rm -f config.json
+base64 -d config > config.json
+rm -f config
 
-# 如果有设置哪吒探针三个变量,会安装。如果不填或者不全,则不会安装
-[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
 
 nginx
-base64 -d config > config.json
-./${RELEASE_RANDOMNESS} -config=config.json
+nohup ./web.sh run ./config.json >/dev/null 2>&1 &
