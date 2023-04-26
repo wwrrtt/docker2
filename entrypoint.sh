f158801f0d2e4704d90ee8192fdaf52e56640948
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-04-26 08:40:57 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-export PORT=${PORT-8080}
+export PORT=${PORT-10000}
 export UUID=${UUID:-'1bb7c055-8790-4argo.sh4-da53-c9f9bcc669bd'}
 export VMESS_WSPATH=${VMESS_WSPATH:-'/ray272449844'}
 
