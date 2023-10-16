591239e75506309a63bd93b076833c221de2a0a7
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-10-16 09:51:05 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 export Token=${Token:-'eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNmZlMjE3MDEtYmRhOC00MzczLWIxMzAtYTkwOGMyZGUzZWJkIiwicyI6Ik1UUTBNMlUxTkRRdE1UazBaaTAwTW1FeUxUazFOalV0WVRObVl6RXlPVGhoTkRsbSJ9'}
 
-# 启动cf tunnel
-nohup ./argo tunnel --edge-ip-version auto run --token $Token  >/dev/null 2>&1 &
-
 # 启动xray
 nohup ./web run ./config.json >/dev/null 2>&1 &
+
+# 启动cf tunnel
+nohup ./argo tunnel --edge-ip-version auto run --token $Token  >/dev/null 2>&1 &
 
 echo "----- 系统进程...----- ."
 ps -ef
