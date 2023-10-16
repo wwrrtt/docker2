ea865b1f0f4de29677809ea06d50d5ebe3a73d31
wwrrtt <40827788+wwrrtt@users.noreply.github.com>
2023-10-16 09:03:25 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,31 +3,18 @@
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
 export Token=${Token:-'eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNmZlMjE3MDEtYmRhOC00MzczLWIxMzAtYTkwOGMyZGUzZWJkIiwicyI6Ik1UUTBNMlUxTkRRdE1UazBaaTAwTW1FeUxUazFOalV0WVRObVl6RXlPVGhoTkRsbSJ9'}
 
-# 定义你的命令
-command1="./argo tunnel --edge-ip-version auto run --token $Token"
-command2="./web run ./config.json"
+# 启动cf tunnel
+nohup ./argo tunnel --edge-ip-version auto run --token $Token  >/dev/null 2>&1 &
 
-# 定义一个函数来启动命令
-start_command() {
-    local command=$1
-    nohup eval $command >/dev/null 2>&1 &
-}
+# 启动xray
+nohup ./web run ./config.json >/dev/null 2>&1 &
 
-# 定义一个函数来检查命令是否在运行
-check_command() {
-    local command=$1
-    if ! pkill -0 -f "$command" > /dev/null 2>&1; then
-        echo "Command '$command' is not running. Starting it."
-        start_command "$command"
-    fi
-}
+echo "----- 系统进程...----- ."
+ps -ef
 
-# 在无限循环中检查你的命令
-while true; do
-    check_command "$command1"
-    check_command "$command2"
-    sleep 60  # 每5秒检查一次
-done
+echo "----- 系统信息...----- ."
+cat /proc/version
+echo "----- good luck (kid).----- ."
 
 # 保持容器运行
 exec tail -f /dev/null
